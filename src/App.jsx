a829af8f2a269beabbcbfa0b5147fb5deee093e4
nhtzeropoint5 <nuzhatafrin5158@gmail.com>
2026-03-28 21:10:55 +0600

    commit 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 
 import './App.css'
+import Navbar from './assets/components/navbar/navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Navbar></Navbar>
+
     </>
   )
 }
---
+++ src/assets/components/navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import dicon from '../../../assets/dollar_1.png';
+
+const Navbar = () => {
+    return (
+ <div className="navbar bg-base-100 shadow-sm">
+  <div className="flex-1">
+    <a className="btn btn-ghost text-xl">daisyUI</a>
+  </div>
+  <div className="flex-none">
+    <button className="flex justify-between items-center gap-2">
+      O Coins
+      <img src={dicon} alt="" />
+    </button>
+  </div>
+</div>
+    );
+};
+
+export default Navbar;
